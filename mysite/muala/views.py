953701bbecc91d6bafae595fb406647576001e23
8uziak <matbuziak@gmail.com>
2023-11-29 22:54:20 +0100

    dodane sciezki, zmiany architektoniczne

--- mysite/muala/views.py
+++ mysite/muala/views.py
@@ -1,6 +1,27 @@
+from django.http import Http404
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the MUALA index.")
+    template = loader.get_template("muala/index.html")
+    return HttpResponse(template.render(request))
+
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, "muala/detail.html", {"question": question})
+
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
